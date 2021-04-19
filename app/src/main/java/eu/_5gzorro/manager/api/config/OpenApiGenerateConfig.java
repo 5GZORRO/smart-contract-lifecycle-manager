183de4d61292abf055784b43ed96a82629415d5d
James Taylor <jamestaylor@bartr.world>
2021-04-19 16:12:07 +0100

    fix(openapi config): added config & mocks required for successful openapi generation

--- app/src/main/java/eu/_5gzorro/manager/api/config/OpenApiGenerateConfig.java
+++ app/src/main/java/eu/_5gzorro/manager/api/config/OpenApiGenerateConfig.java
@@ -2,7 +2,12 @@ package eu._5gzorro.manager.api.config;
 
 import static org.mockito.Mockito.mock;
 
+import eu._5gzorro.manager.api.controller.ServiceLevelAgreementController;
+import eu._5gzorro.manager.api.controller.ServiceLevelAgreementControllerImpl;
+import eu._5gzorro.manager.api.repository.ServiceLevelAgreementRepository;
 import eu._5gzorro.manager.api.service.ProductOfferingService;
+import eu._5gzorro.manager.api.service.ServiceLevelAgreementService;
+import eu._5gzorro.manager.api.service.ServiceLevelAgreementServiceImpl;
 import eu._5gzorro.manager.service.ProductOfferingDriver;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
@@ -32,5 +37,16 @@ public class OpenApiGenerateConfig {
   @Bean
   public ProductOfferingService productOfferingService() {
     return mock(ProductOfferingService.class);
+  }
+
+  @Primary
+  @Bean
+  public ServiceLevelAgreementService serviceLevelAgreementService() {
+    return mock(ServiceLevelAgreementServiceImpl.class);
+  }
+
+  @Bean
+  public ServiceLevelAgreementRepository serviceLevelAgreementRepository() {
+    return mock(ServiceLevelAgreementRepository.class);
   }
 }
