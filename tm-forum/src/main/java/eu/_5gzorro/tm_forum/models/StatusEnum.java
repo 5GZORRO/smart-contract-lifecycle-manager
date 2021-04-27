fa75704286af39101bb4eb3723976432f8b65c46
James Ronayne <jamesronayne1994@gmail.com>
2021-04-27 11:50:18 +0100

    Fix enum deserialization on tm-forum models

--- tm-forum/src/main/java/eu/_5gzorro/tm_forum/models/StatusEnum.java
+++ tm-forum/src/main/java/eu/_5gzorro/tm_forum/models/StatusEnum.java
@@ -1,5 +1,8 @@
 package eu._5gzorro.tm_forum.models;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+
 public enum StatusEnum {
     NOT_STARTED("Not Started"),
 
@@ -15,13 +18,13 @@ public enum StatusEnum {
         this.value = value;
     }
 
-
-
+    @Override
+    @JsonValue
     public String toString() {
         return String.valueOf(value);
     }
 
-
+    @JsonCreator
     public static StatusEnum fromValue(String text) {
         for (StatusEnum b : StatusEnum.values()) {
             if (String.valueOf(b.value).equals(text)) {
