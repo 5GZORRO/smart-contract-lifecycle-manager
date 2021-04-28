d54688a8239ca221b2451366dc18a636ccece56e
James Ronayne <jamesronayne1994@gmail.com>
2021-04-28 10:01:06 +0100

    Adding kafka logging

--- app/src/main/java/eu/_5gzorro/manager/api/service/kafka/ProductOfferingKafkaService.java
+++ app/src/main/java/eu/_5gzorro/manager/api/service/kafka/ProductOfferingKafkaService.java
@@ -3,6 +3,8 @@ package eu._5gzorro.manager.api.service.kafka;
 import eu._5gzorro.manager.domain.events.ProductOfferingUpdateEvent;
 import eu._5gzorro.manager.service.ProductOfferingDriver;
 import io.reactivex.rxjava3.disposables.Disposable;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.kafka.core.KafkaTemplate;
@@ -14,6 +16,7 @@ import javax.annotation.PreDestroy;
 @ConditionalOnProperty("spring.kafka.enabled")
 @Service
 public class ProductOfferingKafkaService extends AbstractProducer<ProductOfferingUpdateEvent> {
+  private static final Logger log = LoggerFactory.getLogger(ProductOfferingKafkaService.class);
   @Value("${spring.kafka.update-topics.product-offering}")
   private String productOfferTopic;
 
@@ -30,14 +33,16 @@ public class ProductOfferingKafkaService extends AbstractProducer<ProductOfferin
 
   @PostConstruct
   public void setup() {
+    log.info("Starting kafka updates for product offerings");
     kafkaPublishDisposable = driver.productOfferObservable()
-        .subscribe(productOfferingUpdateEvent ->
+        .subscribe(productOfferingUpdateEvent -> {
+                log.info("ProductOffering Kafka update: {}", productOfferingUpdateEvent);
                 super.send(
                         productOfferTopic,
                         productOfferingUpdateEvent.getDeduplicationId(),
                         productOfferingUpdateEvent
-                )
-        );
+                );
+        });
   }
 
   @PreDestroy
