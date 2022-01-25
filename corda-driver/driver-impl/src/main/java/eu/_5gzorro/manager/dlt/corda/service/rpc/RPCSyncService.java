eadf3cd5c966ad59ee7cde58a0bef23bf336309b
Mike-no <deangelismichael98@gmail.com>
2022-01-25 13:25:06 +0100

    fixed vault query w/ paging

--- corda-driver/driver-impl/src/main/java/eu/_5gzorro/manager/dlt/corda/service/rpc/RPCSyncService.java
+++ corda-driver/driver-impl/src/main/java/eu/_5gzorro/manager/dlt/corda/service/rpc/RPCSyncService.java
@@ -1,15 +1,23 @@
 package eu._5gzorro.manager.dlt.corda.service.rpc;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.function.Consumer;
+
 import net.corda.core.contracts.ContractState;
 import net.corda.core.contracts.StateAndRef;
 import net.corda.core.messaging.DataFeed;
+import net.corda.core.node.services.Vault;
 import net.corda.core.node.services.Vault.Page;
 import net.corda.core.node.services.Vault.Update;
+import net.corda.core.node.services.vault.PageSpecification;
 import net.corda.core.node.services.vault.QueryCriteria.VaultQueryCriteria;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import rx.functions.Action1;
+
+import static net.corda.core.node.services.vault.QueryCriteriaUtils.DEFAULT_PAGE_NUM;
+import static net.corda.core.node.services.vault.QueryCriteriaUtils.DEFAULT_PAGE_SIZE;
 
 public abstract class RPCSyncService<T extends ContractState> {
   private static final Logger log = LoggerFactory.getLogger(RPCSyncService.class);
@@ -31,9 +39,18 @@ public abstract class RPCSyncService<T extends ContractState> {
       Action1<Update<T>> updateFunction,
       Consumer<StateAndRef<T>> forEachRecordOnStartup
   ) {
-    client.getClient()
-        .vaultQueryByCriteria(criteria, typeParameterClass).getStates()
-        .forEach(forEachRecordOnStartup);
+    int pageNumber = DEFAULT_PAGE_NUM;
+    List<StateAndRef<T>> states = new ArrayList<>();
+    long totalResults;
+    do {
+      PageSpecification pageSpecification = new PageSpecification(pageNumber, DEFAULT_PAGE_SIZE);
+      Vault.Page<T> results = client.getClient().vaultQueryByWithPagingSpec(typeParameterClass, criteria, pageSpecification);
+      totalResults = results.getTotalStatesAvailable();
+      states.addAll(results.getStates());
+      pageNumber++;
+    } while(((long) DEFAULT_PAGE_SIZE * (pageNumber - 1) <= totalResults));
+
+    states.forEach(forEachRecordOnStartup);
 
     DataFeed<Page<T>, Update<T>> dataFeed
         = client.startTracking(typeParameterClass, criteria);
